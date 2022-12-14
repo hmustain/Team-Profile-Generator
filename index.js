e9e13bb4a890d7b511ab5747cdafe2284c7aba0f
hmustain <hunter@bunkerbranding.com>
2022-12-14 15:51:58 -0600

    working on boilerplate for index html file. added framework for html

---
+++ index.js
@@ -0,0 +1,56 @@
+// Include packages needed for this application. Went back to prev readme generator homework for guidance
+const inquirer = require(`inquirer`);
+const fs = require (`fs`);
+
+
+
+
+
+// Create an array of questions for user input using inquirer prompt. Again went back to prev homework on readme generator for guidance. 
+const generateHTML = () => 
+
+`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!-- Compiled and minified CSS link -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+    />
+    <link rel="stylesheet" href="./style.css" />
+    <title>Team Profile Generator</title>
+  </head>
+  <body>
+    <header class="header">
+      <h1 id="header-text"></h1>
+    </header>
+    <div class="container">
+      <div class="row">
+        <div class="col s12 m4 l4">
+          <div class="card emp-card">
+            <div class="card-content emp-title">
+              <span class="card-title emp-name"></span>
+              <ul>
+                <li id="emp-pos"></li>
+              </ul>
+            </div>
+            <div class="card-action emp-info">
+              <ul class="collection">
+                <li class="collection-item"></li>
+                <li class="collection-item">
+                  
+                </li>
+                <li class="collection-item"></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+</html>
+`
